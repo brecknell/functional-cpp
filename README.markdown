e43514581dc52c16a8b6f989bbc6c1c338d2493c
Matthew Brecknell <matthew@brecknell.net>
2012-05-02 17:15:03 +1000

    Added a simple optional-value type

--- README.markdown
+++ README.markdown
@@ -4,32 +4,37 @@
 A library for functional programming in C++, including support for immutable
 algebraic data types.
 
-## Past
+## About
 
-This library was initially developed for a [talk][] given at the [Brisbane
+Functional C++ was initially developed for a [talk][] given at the [Brisbane
 Functional Programming Group][BFPG]. Slides for the talk are available in the
 doc directory.
 
 [BFPG]: http://bfpg.org/events/58050552
 [talk]: http://vimeo.com/41011774
 
-## Present
+For now, the library contains a strict, boxed sum type, a strict, unboxed
+optional-value type, basic pattern-matching, and some simple examples.
 
-For now, there's a strict sum type with basic pattern-matching support, and
-some simple examples.
+In time, I hope to include better pattern matching, lazy evaluation, a
+selection of immutable data structures, useful higher-order abstractions,
+[QuickCheck][]-style testing, and documentation.
 
-## Future
+[QuickCheck]: http://hackage.haskell.org/package/QuickCheck
 
-In time, I hope to include better support for pattern matching, lazy
-evaluation, a selection of immutable data structures, useful higher-order
-abstractions, and documentation.
+## Requirements
+
+This is a C++11 library, so you'll need a recent C++ compiler, building in
+C++11 mode. I am using GCC 4.7.0.
+
+Currently, it is an include-only library, so you just need to ensure your
+compiler can find the files in the include directory.
 
 ## License
 
-Copyright &copy; Matthew Brecknell 2012.
-
-Distributed under the Boost Software License, Version 1.0. See accompanying
-file LICENSE or a copy at [boost.org][boost-license].
+Copyright &copy; Matthew Brecknell 2012. Distributed under the Boost Software
+License, Version 1.0. (See accompanying file LICENSE.txt or a copy at
+[boost.org][boost-license]).
 
 [boost-license]: http://www.boost.org/LICENSE_1_0.txt
 
---
+++ include/option.hpp
@@ -0,0 +1,105 @@
+
+// Copyright (c) Matthew Brecknell 2012.
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE.txt or a copy at http://www.boost.org/LICENSE_1_0.txt).
+
+//
+// option<T>
+//
+// An immutable, strict, unboxed, optional value type.
+//
+
+#include <type_traits>
+#include <utility>
+
+template <typename T>
+class option final {
+
+  bool full;
+
+  // Union avoids automatic construction of value.
+  union { T value; };
+
+  struct some_tag {};
+
+  template <typename... Args>
+  option(some_tag, Args &&... args)
+    : full(true), value(std::forward<Args>(args)...) {}
+
+public:
+
+  option() : full(false) {}
+
+  option(const option & other) : full(other.full) {
+    if (full) { new (&value) T(other.value); }
+  }
+
+  option(option && other) : full(other.full) {
+    if (full) { new (&value) T(std::move(other.value)); }
+  }
+
+  option & operator = (const option &) = delete;
+  option & operator = (option &&) = delete;
+
+  option(const T & value)
+    : full(true), value(value) {}
+
+  option(T && value)
+    : full(true), value(std::move(value)) {}
+
+  // Factory function which constructs the value in-place.
+
+  template <typename... Args>
+  static option some(Args &&... args) {
+    return option(some_tag(), std::forward<Args>(args)...);
+  }
+
+  // Perform a computation: option<T> ==> R,
+  //
+  // Arguments:
+  // - f :: T ==> R, in case the option<T> contains a value,
+  // - n ::   ==> R, in case the option<T> is empty.
+
+  template <typename F, typename N>
+  typename
+    std::common_type<
+      typename std::result_of<F(T)>::type,
+      typename std::result_of<N()>::type
+    >::type
+  elim(F && f, N && n) const {
+    return full ? f(value) : n();
+  }
+
+  // Perform a computation: option<T> ==> R,
+  //
+  // Argument:
+  // - f :: T ==> R, in case the option<T> contains a value.
+  //
+  // Requirement:
+  // - R must be default-constructible, in case the option<T> is empty.
+  //
+  // Useful for chaining computations on optional values.
+
+  template <typename F>
+  typename std::result_of<F(T)>::type
+  operator >> (F && f) const {
+    return full ? f(value) : typename std::result_of<F(T)>::type();
+  }
+
+  ~option() {
+    if (full) { value.~T(); }
+  }
+
+};
+
+template <typename T, typename... Args>
+option<T> some(Args &&... args) {
+  return option<T>::some(std::forward<Args>(args)...);
+}
+
+template <typename T>
+option<T> none() {
+  return option<T>();
+}
+
